69aeb0a7e2f09f98fbbf22257f455b0fcd216ae6
Vinhta6868 <56138390+Vinhta6868@users.noreply.github.com>
2019-10-03 21:47:09 -0700

    Second version

--- CSE360_Assignment2/src/cse360assign2/AddingMachine.java
+++ CSE360_Assignment2/src/cse360assign2/AddingMachine.java
@@ -10,28 +10,31 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private String history = "0";   // Kept as string history to be returned in getString 
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
+		history += (" + " + value);  // update history
 	}
 	
 	public void subtract (int value) {
-		
+		total -= value;
+		history += (" - " + value);  // update history
 	}
 		
 	public String toString () {
-		return "";
+		return history;
 	}
 
 	public void clear() {
-	
+		history = "0";   // clear/reset history
 	}
 }
